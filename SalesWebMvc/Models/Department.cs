9d6eb06165a5502ff40cdd155a2aea7630c3b318
Neberson Andrade <neberson_mtv@hotmail.com>
2023-01-31 21:57:06 -0300

    Realizado a criação do atributo sellers, para listar os vendedores do departamento e realizado a criação do metodo TotalSales para totalizar as vendas do departamento

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,38 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        private ICollection<Seller> _sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            _sellers.Add(seller);
+        }
+
+        public void RemoveSeller(Seller seller)
+        {
+            _sellers.Remove(seller);
+        }
+
+        public double TotalSales(DateTime dateInitial, DateTime dateFinal)
+        {
+            return _sellers.Sum(seller => seller.TotalSales(dateInitial, dateFinal));
+        }
     }
 }
