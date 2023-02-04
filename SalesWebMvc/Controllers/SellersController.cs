b563a000449f3d6ccd175389603eb57a19054469
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-04 00:48:40 -0300

    Realizado ajuste no SellersController para listar todos os vendedores. Realizado a criação de uma ação Create para acessar a tela de cadastro dos vendedores. Realizado a criação de uma ação Create para receber os dados da view Create Seller e efetivar o cadastro do vendedor.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
+            var sellers = _sellerService.FindAll();
+            return View(sellers);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
