5e2c940e58b1eeabecdad4a2e2922ee1b60b7846
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-05 11:24:43 -0300

    Realizado a criação dos metodos FindById e Remove no SellerService, para retornar um vendedor por id e fazer a deleção de um vendedor.

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -2,6 +2,8 @@
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -23,6 +25,37 @@ namespace SalesWebMvc.Services
         {
             _context.Add(seller);
             _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(seller => seller.Department).FirstOrDefault(seller => seller.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            if (!_context.Seller.Any(sellerBanco => sellerBanco.Id == seller.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+
         }
 
     }
