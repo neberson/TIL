a140efb7edc244e6117e7a8e9809eeb04e8bc2ab
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-11 17:47:24 -0300

    Realizado a parametrização de annotions para os atributos das classes Categoria e Lanche, para criação correta das tabelas no banco de dados ao rodar o migration.

--- C-Sharp/LanchesMac/Models/Lanche.cs
+++ C-Sharp/LanchesMac/Models/Lanche.cs
@@ -1,18 +1,54 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        #region "atributos da classe"
+        [Key]
         public int LancheId { get; set; }
-        public string? Nome { get; set; }
-        public string? DescricaoCurta { get; set; }
-        public string? DescricaoDetalhada { get; set; }
-        public decimal Preco { get; set; }
-        public string? ImagemUrl { get; set; }
-        public string? ImagemThumbnailUrl { get; set; }
-        public bool IsLanchePreferido { get; set; }
-        public bool EmEstoque { get; set; }
 
+        [Required(ErrorMessage = "Informe o nome do Lanche")]
+        [Display(Name = "Nome do Lanche")]
+        [StringLength(80, MinimumLength = 5, ErrorMessage = "O {0} deve ter no mínimo {2} e no máximo {1}")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "A descriação do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "A {0} deve ter no mínimo {2} e no máximo {1}")]
+        public string? DescricaoCurta { get; set; }
+
+        [Required(ErrorMessage = "A descriação detalhada do lanche deve ser informada")]
+        [Display(Name = "Descrição detalhada do Lanche")]
+        [StringLength(500, MinimumLength = 20, ErrorMessage = "A {0} deve ter no mínimo {2} e no máximo {1}")]
+        public string? DescricaoDetalhada { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do lanche")]
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0.01, 999.99, ErrorMessage = "O preço deve estar entre 0,01 e 999,99")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Caminho Imagem Normal")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        public string? ImagemUrl { get; set; }
+
+        [Display(Name = "Caminho Imagem Miniatura")]
+        [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+        public string? ImagemThumbnailUrl { get; set; }
+
+        [Display(Name = "Preferido?")]
+        public bool IsLanchePreferido { get; set; }
+
+        [Display(Name = "Estoque")]
+        public bool EmEstoque { get; set; }
+        #endregion
+
+        #region "Relacionamento com a tabela categorias"
         public int CategoriaId { get; set; }
-        public virtual Categoria Categoria { get; set; }
+        public virtual Categoria? Categoria { get; set; }
+        #endregion
     }
 }
