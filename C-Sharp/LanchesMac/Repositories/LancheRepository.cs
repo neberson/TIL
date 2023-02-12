3e89c314cce5f01c4aeea4fa3ad4b41802b08f68
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-12 17:55:55 -0300

    Realizado a implementação do padrão Repository para listar os Lanches.

---
+++ C-Sharp/LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories;
+public class LancheRepository : ILanchesRepository
+{
+    private readonly AppDbContext _context;
+    public LancheRepository(AppDbContext contexto)
+    {
+        _context = contexto;
+    }
+
+    public IEnumerable<Lanche> Lanches => _context.Lanches.Include(lanche => lanche.Categoria);
+
+    public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches
+                                                            .Where(lanche => lanche.IsLanchePreferido == true)
+                                                            .Include(lanche=> lanche.Categoria);
+
+    public Lanche GetLancheById(int lancheId)
+    {
+        return _context.Lanches
+                       .Include(lanche => lanche.Categoria)
+                       .FirstOrDefault(lanche => lanche.LancheId == lancheId, new Lanche());
+    }
+}
