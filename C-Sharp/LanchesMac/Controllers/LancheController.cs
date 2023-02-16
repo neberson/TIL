c4ad6f5a43aa0134da370e84305555e846df462a
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-15 23:53:03 -0300

    Realizado a criação da ViewModel "LancheListViewModel" para definir um modelo a ser utilizado na visão.

--- C-Sharp/LanchesMac/Controllers/LancheController.cs
+++ C-Sharp/LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers;
@@ -13,7 +14,13 @@ public class LancheController : Controller
 
     public IActionResult List()
     {
-        var lanches = _lancheRepository.Lanches;
-        return View(lanches);
+        // var lanches = _lancheRepository.Lanches;
+        //return View(lanches);
+
+        var lanchesListViewModel = new LancheListViewModel();
+        lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+        lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+        return View(lanchesListViewModel);
     }
 }
