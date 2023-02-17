ec3694ebcba823e9ad938ef5aab7ba996af6aae2
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-16 22:48:58 -0300

    Realizado a criação da entidade CarrinhoCompraItem.

---
+++ C-Sharp/LanchesMac/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LanchesMac.Models;
+
+[Table("CarrinhoCompraItens")]
+public class CarrinhoCompraItem
+{
+    [Key]
+    public int CarrinhoCompraItemId { get; set; }
+    [Required(ErrorMessage = "Informe o lanche")]
+    public Lanche? Lanche { get; set; }
+    [Required(ErrorMessage = "Informe a quantidade")]
+    [MinLength(1,ErrorMessage = "A quantidade mínima para item é {1}")]
+    public int Quantidade { get; set; }
+    [StringLength(100)]
+    public string? CarrinhoCompraId { get; set; }
+
+}
