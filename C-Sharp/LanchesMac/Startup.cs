c81ef75baf2ff933e244dbf015a03979b705db08
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-21 22:15:47 -0300

    Realizado a configuração do carrinho de compras no arquivo Startup para que ocorra a injenção de dependencia.

--- C-Sharp/LanchesMac/Startup.cs
+++ C-Sharp/LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.AspNetCore.Http;
@@ -22,6 +23,7 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<ILanchesRepository, LancheRepository>();
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped(serviceProvider => CarrinhoCompra.ObtemCarrinho(serviceProvider));
 
         services.AddControllersWithViews();
 
