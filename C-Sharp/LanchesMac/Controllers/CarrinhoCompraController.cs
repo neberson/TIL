09919c3c19d8997aba62888c6c8779e069bffa19
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-21 22:16:21 -0300

    Realizado a criação do controller para gerenciar as ações do carrinho de compras.

---
+++ C-Sharp/LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+public class CarrinhoCompraController : Controller
+{
+    private readonly ILanchesRepository _branchesRepository;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public CarrinhoCompraController(ILanchesRepository branchesRepository, CarrinhoCompra carrinhoCompra)
+    {
+        _branchesRepository = branchesRepository;
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
