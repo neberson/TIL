9c57ab01313f96b930a9f117b0ea521c633e1f7c
Neberson Andrade <neberson_mtv@hotmail.com>
2023-02-28 23:17:02 -0300

    Realizado a criação de uma view component para apresentar o icone do carrinho no menu do sistema.

---
+++ C-Sharp/LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+namespace LanchesMac.Components;
+public class CarrinhoCompraResumo : ViewComponent
+{
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+    {
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        _carrinhoCompra.CarrinhoCompraItens = _carrinhoCompra.ObtemCarrinhoCompraItens();
+
+        var carrinhoCompraVM = new CarrinhoCompraViewModel
+        {
+            CarrinhoCompra = _carrinhoCompra
+        };
+
+        return View(carrinhoCompraVM);
+    }
+}
