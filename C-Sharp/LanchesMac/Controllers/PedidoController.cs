73e8f41fe477b750e7d00885c2a56de59e9d3da5
Neberson Andrade <neberson_mtv@hotmail.com>
2023-03-10 23:42:21 -0300

    Criado o controller do pedido.

---
+++ C-Sharp/LanchesMac/Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompraController _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompraController carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Chechout(Pedido pedido) 
+        {
+            return View();
+        }
+    }
+}
