d505e4796e467340fb5efaee00077bd44a49409f
Neberson Andrade <neberson_mtv@hotmail.com>
2023-03-16 00:07:00 -0300

    Realizado a criação das validações e gravação dos dados do pedido, ao realizar a conclusão do pedido.

--- C-Sharp/LanchesMac/Controllers/PedidoController.cs
+++ C-Sharp/LanchesMac/Controllers/PedidoController.cs
@@ -24,7 +24,39 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) 
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            _carrinhoCompra.CarrinhoCompraItens = _carrinhoCompra.ObtemCarrinhoCompraItens();
+
+            if(_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio, que tal incluir um lanche...");
+            }
+
+            foreach(CarrinhoCompraItem item in _carrinhoCompra.CarrinhoCompraItens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += ( item.Lanche.Preco * item.Quantidade);
+
+            }
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            if(ModelState.IsValid)
+            {
+                _pedidoRepository.CriarPedido(pedido);
+
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.ObtemCarrinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+
+            return View(pedido);
         }
     }
 }
