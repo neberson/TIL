5e9748cd8c00471cd86e6f19b887d0f5520a99d7
Neberson Andrade <neberson_mtv@hotmail.com>
2023-03-21 00:15:49 -0300

    Realizado a criação da viewModel de login e controller da conta do usuário.

---
+++ C-Sharp/LanchesMac/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers;
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public IActionResult Login(string returnUrl)
+    {
+        return View(new LoginViewModel()
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+    {
+        if (!ModelState.IsValid)
+           return View(loginViewModel);
+
+        var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+        if (user != null)
+        {
+            var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+            if (result.Succeeded)
+            {
+                if(string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(loginViewModel.ReturnUrl);
+            }
+        }
+        ModelState.AddModelError("", "Falha ao realizar o login!!");
+        return View(loginViewModel);
+    }
+}
