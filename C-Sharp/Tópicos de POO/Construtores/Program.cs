8069b18f5f7610b8864d295455a2407f1ebe8444
Neberson Andrade <neberson_mtv@hotmail.com>
2023-03-24 00:14:23 -0300

    Realizado implementação das instancia do artigo de Construtores.

--- C-Sharp/Tópicos de POO/Construtores/Program.cs
+++ C-Sharp/Tópicos de POO/Construtores/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Construtores
 {
-    internal class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.Nome = "Paulo";
+            pessoa1.Idade = 45;
+            pessoa1.Genero = "Masculino";
+            pessoa1.Identificar();
+
+            Pessoa pessoa2 = new Pessoa("Paulo", 45, "Masculino");
+            pessoa2.Identificar();
+
+            ContaBancaria conta = new ContaBancaria("João Batista");
+            conta.Depositar(100);
+            conta.Sacar(50);
+            conta.MostrarDetalhes();
         }
     }
 }
