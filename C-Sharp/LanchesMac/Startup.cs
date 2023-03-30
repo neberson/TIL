d759e642e6f5337d11995f9685cb412bd8de5c61
Neberson Andrade <neberson_mtv@hotmail.com>
2023-03-29 23:42:22 -0300

    Criação dos perfis Admin e Member, para bloqueio da Área de administração do sistema. Sendo permitido apenas usuários com perfil de Admin.

--- C-Sharp/LanchesMac/Startup.cs
+++ C-Sharp/LanchesMac/Startup.cs
@@ -2,6 +2,7 @@
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -38,6 +39,17 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<ILanchesRepository, LancheRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+        services.AddAuthorization(options => 
+        {
+            options.AddPolicy("Admin",
+                politica => 
+                {
+                    politica.RequireRole("Admin");
+                });
+        });
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped(serviceProvider => CarrinhoCompra.ObtemCarrinho(serviceProvider));
 
@@ -48,7 +60,7 @@ public class Startup
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ISeedUserRoleInitial seedUserRoleInitial)
     {
         if (env.IsDevelopment())
         {
@@ -62,6 +74,9 @@ public class Startup
         }
         app.UseHttpsRedirection();
         app.UseStaticFiles();
+
+        seedUserRoleInitial.SeedRoles();
+        seedUserRoleInitial.SeedUsers();
 
         app.UseRouting();
         app.UseSession();
