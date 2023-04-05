ee216d9d1d82f9ed28701108357b1cb60d82329a
Neberson Andrade <neberson_mtv@hotmail.com>
2023-04-04 23:16:39 -0300

    Realizado a criação de um relatório de vendas para o sistema.

--- C-Sharp/LanchesMac/Startup.cs
+++ C-Sharp/LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Context;
+﻿using LanchesMac.Areas.Admin.Services;
+using LanchesMac.Context;
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
@@ -40,6 +41,7 @@ public class Startup
         services.AddTransient<ILanchesRepository, LancheRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+        services.AddScoped<RelatorioVendasService>();
 
         services.AddAuthorization(options => 
         {
