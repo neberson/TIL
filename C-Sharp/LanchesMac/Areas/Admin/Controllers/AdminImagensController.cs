1030e5c99c9a9266ec6d689b8a5c5410d40aa34b
Neberson Andrade <neberson_mtv@hotmail.com>
2023-04-10 23:42:53 -0300

    Realizado implementação para upload de imagens e view para confirmação do upload.

--- C-Sharp/LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
+++ C-Sharp/LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -23,5 +23,47 @@ namespace LanchesMac.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Error: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Error: Quantidade de arquivos excedeu o limite (É permitido no máximo 10 arquivos).";
+                return View(ViewData);
+            }
+
+            long size = files.Sum(f => f.Length);
+
+            var filePathsName = new List<string>();
+
+            var filePath = Path.Combine(_webHostEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos);
+
+            foreach (var file in files)
+            {
+                if (file.FileName.Contains(".jpg") || file.FileName.Contains(".gif") || file.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", file.FileName);
+
+                    filePathsName.Add(fileNameWithPath);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor, " + 
+                                    $"com tamanho total de : {size} bytes";
+            ViewBag.Arquivos = filePathsName;
+
+            return View(ViewData);
+        }
     }
 }
