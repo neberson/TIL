4867060c10bb5040529360b7f38592fc900eac40
Neberson Andrade <neberson_mtv@hotmail.com>
2023-04-11 23:54:45 -0300

    Criado código de exemplo após estudo da palabra reservada this em c#.

---
+++ C-Sharp/Tópicos de POO/PalavraReservadaThis/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿namespace PalavraReservadaThis;
+partial class Program
+{
+    class Pessoa
+    {
+        //definição dos campos/atributos da classe
+        public string Nome;
+        public int Idade;
+        public string Genero;
+        /*Construtor base da classe Pessoa*/
+        public Pessoa(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        /*Sobrecarga do construtor da classe Pessoa utilizando a palavra this para atribuir valor ao nome através do construtor base*/
+        public Pessoa(string nome, int idade, string genero) : this(nome)
+        {
+            this.Idade  = idade;
+            this.Genero = genero;
+        }
+
+        //métodos refletem o comportamento e ações da classe
+        public void Identificar()
+        {
+            System.Console.WriteLine($"Olá, sou o {Nome} tenho {Idade} e sou do sexo {Genero}");
+        } 
+    }
+}
---
+++ C-Sharp/Tópicos de POO/PalavraReservadaThis/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PalavraReservadaThis
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exemplo utilizando o construtor base
+            Pessoa pessoa1 = new Pessoa("Lorraine");
+
+            pessoa1.Idade = 32;
+            pessoa1.Genero = "Feminino";
+            pessoa1.Identificar();
+
+            //Exemplo utilizando a sobrecarga do construtor com a palabra reservada THIS
+            Pessoa pessoa2 = new Pessoa("Davi", 20, "Masculino");
+            pessoa2.Identificar();
+        }
+    }
+}
