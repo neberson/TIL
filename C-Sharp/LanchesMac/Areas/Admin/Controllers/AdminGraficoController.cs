b621e9ac3a4da310860a8bb8790fa14524e25e36
Neberson Andrade <neberson_mtv@hotmail.com>
2023-04-13 23:58:42 -0300

    Realizado a implementação das ações para gerar os gráficos com Google Chart.

---
+++ C-Sharp/LanchesMac/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,42 @@
+﻿using LanchesMac.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    public class AdminGraficoController : Controller
+    {
+        private readonly GraficoVendasServico _graficoVendasServico;
+
+        public AdminGraficoController(GraficoVendasServico graficoVendasServico)
+        {
+            _graficoVendasServico = graficoVendasServico ?? throw
+                                    new ArgumentNullException(nameof(graficoVendasServico));
+        }
+
+        public JsonResult VendasLanches(int dias)
+        {
+            var lanchesVendasTotais = _graficoVendasServico.GetVendasLanches();
+
+            return Json(lanchesVendasTotais);
+        }
+
+        [HttpGet]
+        public IActionResult Index(int dias)
+        {
+            return View();
+        }        
+        
+        [HttpGet]
+        public IActionResult VendasMensal(int dias)
+        {
+            return View();
+        }        
+        
+        [HttpGet]
+        public IActionResult VendasSemanal(int dias)
+        {
+            return View();
+        }
+    }
+}
---
+++ C-Sharp/LanchesMac/Areas/Admin/Services/GraficoVendasServico.cs
@@ -0,0 +1,34 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+
+namespace LanchesMac.Areas.Admin.Services
+{
+    public class GraficoVendasServico
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public GraficoVendasServico(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public List<LancheGrafico> GetVendasLanches(int dias = 360)
+        {
+            var data = DateTime.Now.AddDays(-dias);
+
+            var lanchesGrafico = (from pd in _appDbContext.PedidoDetalhes
+                                 join l in _appDbContext.Lanches on pd.LancheId equals l.LancheId
+                                 where pd.Pedido.PedidoEnviado >= data
+                                 group pd by new { pd.LancheId, l.Nome, pd.Quantidade}
+                                 into g
+                                 select new LancheGrafico
+                                {
+                                    LancheNome = g.Key.Nome,
+                                    LanchesQuantidade = g.Sum(q => q.Quantidade),
+                                    LanchesValorTotal = g.Sum(a => a.Preco * a.Quantidade)
+                                }).ToList();
+
+            return lanchesGrafico;
+        }
+    }
+}
