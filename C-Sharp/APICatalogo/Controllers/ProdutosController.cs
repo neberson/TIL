faaa8f4f2ee4776ba96737147f3ceb1b6f6c947c
Neberson Andrade <neberson_mtv@hotmail.com>
2023-05-02 23:32:15 -0300

    Criado o controller de produtos com os metodos get para retornar todos os produtos e produtos por id.

---
+++ C-Sharp/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+    }
+}
