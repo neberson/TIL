49a063de32ea0e459f3b803de0994ccadfa67294
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-14 23:11:29 -0300

    Implementado um menu para a calculadora

--- C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
@@ -5,10 +5,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Soma();
-        //Subtracao();
-        //Divisao();
-        Multiplicacao();
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que deseja fazer?");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Divisão");
+        Console.WriteLine("4 - Multiplicação");
+        Console.WriteLine("5 - Sair");
+        Console.WriteLine("-----------------------");
+
+        Console.WriteLine("Selecione uma opção: ");
+        short res = short.Parse(Console.ReadLine());
+
+        switch (res)
+        {
+            case 1: Soma(); break;
+            case 2: Subtracao(); break;
+            case 3: Divisao(); break;
+            case 4: Multiplicacao(); break;
+            case 5: System.Environment.Exit(0); break;
+            default: Menu(); break;
+        }
     }
 
     static void Soma()
@@ -26,6 +48,8 @@ class Program
         float resultado = v1 + v2;
         Console.WriteLine($"O resultado da soma é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 
     static void Subtracao()
@@ -44,6 +68,8 @@ class Program
 
         Console.WriteLine($"O resultado da subtração é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 
     static void Divisao()
@@ -62,6 +88,8 @@ class Program
 
         Console.WriteLine($"O resultado da divisão é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 
     static void Multiplicacao()
@@ -79,5 +107,7 @@ class Program
         Console.WriteLine("");
         Console.WriteLine($"O resultado da multiplicação é {resultado}");
         Console.ReadKey();
+
+        Menu();
     }
 }
