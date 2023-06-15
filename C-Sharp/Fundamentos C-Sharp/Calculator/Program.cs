12b119d7eb305d3affe490a519f3654b37c73a13
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-14 22:54:20 -0300

    Implementado a função de multiplicação

--- C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
@@ -7,7 +7,8 @@ class Program
     {
         //Soma();
         //Subtracao();
-        Divisao();
+        //Divisao();
+        Multiplicacao();
     }
 
     static void Soma()
@@ -63,4 +64,20 @@ class Program
         Console.ReadKey();
     }
 
+    static void Multiplicacao()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 * v2;
+
+        Console.WriteLine("");
+        Console.WriteLine($"O resultado da multiplicação é {resultado}");
+        Console.ReadKey();
+    }
 }
