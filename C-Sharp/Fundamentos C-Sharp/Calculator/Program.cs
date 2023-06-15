15f2c3f1b478c0441c1ae941fdd2b01ec96daf61
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-14 22:35:55 -0300

    Realizado implementação da função de soma

--- C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
@@ -1,8 +1,26 @@
-﻿namespace Calculator;
+﻿using System;
+
+namespace Calculator;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Soma();
+    }
+
+    static void Soma()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+
+        float resultado = v1 + v2;
+        Console.WriteLine($"O resultado da soma é {resultado}");
     }
 }
