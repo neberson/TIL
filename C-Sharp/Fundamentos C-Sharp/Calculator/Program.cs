6dfaf872cd6310af7d40bf234ddf5ff27836f3b1
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-14 22:43:10 -0300

    Implementado a função de subtração

--- C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/Calculator/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Soma();
+        //Soma();
+        Subtracao();
     }
 
     static void Soma()
@@ -18,9 +19,28 @@ class Program
         Console.WriteLine("Segundo valor: ");
         float v2 = float.Parse(Console.ReadLine());
 
-        Console.WriteLine();
+        Console.WriteLine("");
 
         float resultado = v1 + v2;
         Console.WriteLine($"O resultado da soma é {resultado}");
+        Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+        Console.Clear();
+
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 - v2;
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"O resultado da subtração é {resultado}");
+        Console.ReadKey();
     }
 }
