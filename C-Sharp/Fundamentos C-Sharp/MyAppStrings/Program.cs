404013d56ec0654f17c72ea017ac4a5fdef5842a
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-16 00:32:35 -0300

    Implementado função para concatenação de strings

--- C-Sharp/Fundamentos C-Sharp/MyAppStrings/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/MyAppStrings/Program.cs
@@ -6,7 +6,8 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            ImplmentandoGuid();
+            //ImplmentandoGuid();
+            ConcatenarStrings();
         }
 
         static void ImplmentandoGuid()
@@ -25,6 +26,22 @@ namespace MyApp
             //Cria um guid com todas as posições zeradas
             id = new Guid();
             Console.WriteLine(id);
+        }
+        static void ConcatenarStrings()
+        {
+            var price = 10.2;
+
+            //Concatenação
+            var text = "O preço do produto é " + price + " apenas na promoção";
+            Console.WriteLine(text);
+
+            //Placeholders
+            var text2 = string.Format("O preço do produto é {0} apenas na promoção", price);
+            Console.WriteLine(text2);
+
+            //Interpolação
+            var text3 = $"O preço do produto é {price} apenas na promoção";
+            Console.WriteLine(text3);
         }
     }
 }
