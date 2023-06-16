7e17304fb24afcb71a074bd66ca9cf7c7f63d367
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-15 23:54:55 -0300

    Implementado função para geração de guid

--- C-Sharp/Fundamentos C-Sharp/MyAppStrings/Program.cs
+++ C-Sharp/Fundamentos C-Sharp/MyAppStrings/Program.cs
@@ -6,7 +6,25 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            ImplmentandoGuid();
+        }
+
+        static void ImplmentandoGuid()
+        {
+            //Gera sempre um guid único
+            var id = Guid.NewGuid();
+
+            //Pode ser realizar a conversão de um tipo Guid para String
+            id.ToString();
+
+            //Instância um objeto do tipo Guid com os valores informados
+            id = new Guid("74f942f0-6a83-4e46-9470-0d68442763a4");
+
+            Console.WriteLine(id);
+
+            //Cria com guid com todas posições zeradas
+            id = new Guid();
+            Console.WriteLine(id);
         }
     }
 }
