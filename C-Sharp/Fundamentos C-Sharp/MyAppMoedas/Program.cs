9956418110bea0b2d7a2dd12b3c4165ccff201ba
Neberson Andrade <neberson_mtv@hotmail.com>
2023-06-22 00:10:14 -0300

    Criado projeto para estudo sobre Moedas e implementado função com os principais tipos para moedas.

---
+++ C-Sharp/Fundamentos C-Sharp/MyAppMoedas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyAppMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            TiposParaMoedas();
+        }
+
+        static void TiposParaMoedas()
+        {
+            // Cria com formato double
+            var valor = 10.25;
+            Console.WriteLine(valor);
+
+            //É o mais indicado por causa da precisão, mas tem que ser analisado o tamanho do dado
+            decimal valorDecimal = 1000.80M;
+            Console.WriteLine(valorDecimal);
+        }
+    }
+}
