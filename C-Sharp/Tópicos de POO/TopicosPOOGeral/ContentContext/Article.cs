4bfa8919a78eb5c15b93ad9f2f7e3fde2472fad1
Neberson Andrade <neberson_mtv@hotmail.com>
2023-10-28 00:02:29 -0300

    Realizado a implementação dos construtores nas classes Realizado a implementação de interações com listas

--- C-Sharp/Tópicos de POO/TopicosPOOGeral/ContentContext/Article.cs
+++ C-Sharp/Tópicos de POO/TopicosPOOGeral/ContentContext/Article.cs
@@ -2,6 +2,9 @@ namespace TopicosPooGeral.ContentContext
 {
     public class Article : Content
     {
+        public Article(string title, string url) : base(title, url)
+        {
 
+        }
     }
 }
--- C-Sharp/Tópicos de POO/TopicosPOOGeral/Program.cs
+++ C-Sharp/Tópicos de POO/TopicosPOOGeral/Program.cs
@@ -4,9 +4,48 @@ class Program
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Hello World");
+        var articles = new List<Article>();
 
-        var course = new Course();
-        course.Level = ContentContext.Enums.EContentLevel.Beginner;
+        articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+        articles.Add(new Article("Artigo sobre C#", "csharp"));
+        articles.Add(new Article("Artigo sobre .NET", "dotnet"));
+
+        foreach (var article in articles)
+        {
+            Console.WriteLine(article.Id);
+            Console.WriteLine(article.Title);
+            Console.WriteLine(article.Url);
+        }
+        var courses = new List<Course>();
+
+        var courseOOP = new Course("Fundamentos OOP", "fundamentos-OOP");
+        var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+        var courseAspNet = new Course("Fundamentos OOP", "fundamentos-aspnet");
+
+        courses.Add(courseOOP);
+        courses.Add(courseCsharp);
+        courses.Add(courseAspNet);
+
+        var careers = new List<Career>();
+        var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
+
+        var careerItem2 = new CareerItem(2, "Aprenda OOP", "", null);
+        var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+        var careerItem3 = new CareerItem(3, "Aprenda .NET", "", null);
+
+        careerDotNet.Items.Add(careerItem2);
+        careerDotNet.Items.Add(careerItem);
+        careerDotNet.Items.Add(careerItem3);
+
+        careers.Add(careerDotNet);
+
+        foreach (var career in careers)
+        {
+            Console.WriteLine(career.Title);
+            foreach (var item in career.Items.OrderBy(x => x.Order))
+            {
+                Console.WriteLine($"{item.Order} - {item.Title}");
+            }
+        }
     }
 }
