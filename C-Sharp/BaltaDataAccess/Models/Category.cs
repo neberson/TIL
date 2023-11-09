af3f6477d14a624aa04cb7d55625d0d970d1a08d
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-08 23:34:06 -0300

    Realizado a implementação de um insert no banco de dados utilizando Dapper.

--- C-Sharp/BaltaDataAccess/Models/Category.cs
+++ C-Sharp/BaltaDataAccess/Models/Category.cs
@@ -4,5 +4,10 @@ namespace BaltaDataAccess.Models
     {
         public Guid Id { get; set; }
         public String? Title { get; set; }
+        public String? Url { get; set; }
+        public String? Summary { get; set; }
+        public int Order { get; set; }
+        public String? Description { get; set; }
+        public bool Featured { get; set; }
     }
 }
--- C-Sharp/BaltaDataAccess/Program.cs
+++ C-Sharp/BaltaDataAccess/Program.cs
@@ -11,12 +11,46 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=SA;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
+            var category = new Category();
+
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                                    Category 
+                                VALUES(
+                                         @Id, 
+                                         @Title, 
+                                         @Url, 
+                                         @Summary, 
+                                         @Order, 
+                                         @Description, 
+                                         @Featured)";
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
             Console.WriteLine("Hello World!");
