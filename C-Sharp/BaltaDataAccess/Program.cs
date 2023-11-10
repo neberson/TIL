7aa3e84f8ded450627d78a28faa44c3594e0ff00
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-09 23:00:38 -0300

    Realizado a criação de um CRUD separando os metodos. Realizado a implementação de um insert many.

--- C-Sharp/BaltaDataAccess/Program.cs
+++ C-Sharp/BaltaDataAccess/Program.cs
@@ -11,6 +11,29 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=SA;Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ListCategories(connection);
+                //CreateCategory(connection);
+                //UpdateCategory(connection);
+                CreateManyCategory(connection);
+                //DeleteCategory(connection);
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
 
             category.Id = Guid.NewGuid();
@@ -31,10 +54,76 @@ namespace BaltaDataAccess
                                          @Order, 
                                          @Description, 
                                          @Featured)";
-
-            using (var connection = new SqlConnection(connectionString))
+            var rows = connection.Execute(insertSql, new
             {
-                var rows = connection.Execute(insertSql, new
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateCategory = "UPDATE Category SET Title=@title WHERE Id=@id";
+            var rows = connection.Execute(updateCategory, new
+            {
+                id = new Guid("bb69989d-91c5-4f59-8488-5e640d943f5c"),
+                title = "Frontend 2021"
+            });
+            Console.WriteLine($"{rows} registros atualizados");
+        }
+
+        static void DeleteCategory(SqlConnection connection)
+        {
+            var deleteCategory = "DELETE FROM Category WHERE Id=@id";
+            var rows = connection.Execute(deleteCategory, new
+            {
+                id = new Guid("bb69989d-91c5-4f59-8488-5e640d943f5c"),
+                title = "Frontend 2021"
+            });
+            Console.WriteLine($"{rows} registros deletados");
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "categoria-nova";
+            category2.Description = "Categoria nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria";
+            category2.Featured = true;
+
+            var insertSql = @"INSERT INTO 
+                                    Category 
+                                VALUES(
+                                         @Id, 
+                                         @Title, 
+                                         @Url, 
+                                         @Summary, 
+                                         @Order, 
+                                         @Description, 
+                                         @Featured)";
+            var rows = connection.Execute(insertSql, new[]
+            {
+                new
                 {
                     category.Id,
                     category.Title,
@@ -43,17 +132,20 @@ namespace BaltaDataAccess
                     category.Order,
                     category.Description,
                     category.Featured
-                });
-
-                Console.WriteLine($"{rows} linhas inseridas");
-
-                var categories = connection.Query<Category>("SELECT Id, Title FROM Category");
-                foreach (var item in categories)
+                },
+                new
                 {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
-            Console.WriteLine("Hello World!");
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                },
+            });
+
+            Console.WriteLine($"{rows} linhas inseridas");
         }
     }
 }
