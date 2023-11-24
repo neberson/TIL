f1085bfb8d6112f87861e5fd404014c00cfc1338
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-23 22:58:00 -0300

    Realizado a implementação do conceito OneToOne para consulta com JOIN.

--- C-Sharp/BaltaDataAccess/Program.cs
+++ C-Sharp/BaltaDataAccess/Program.cs
@@ -23,8 +23,8 @@ namespace BaltaDataAccess
                 //ExecuteProcedure(connection);
                 //ExecuteReadProcedure(connection);
                 //ExecuteScalar(connection);
-                ReadView(connection);
-
+                //ReadView(connection);
+                OneToOne(connection);
             }
         }
 
@@ -218,6 +218,25 @@ namespace BaltaDataAccess
             foreach (var item in courses)
             {
                 Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+        static void OneToOne(SqlConnection connection)
+        {
+            var sql = @"SELECT 
+                            * 
+                        FROM 
+                            [CareerItem] 
+                        INNER JOIN 
+                            [Course] ON [CareerItem].[CourseId] = [Course].[Id]";
+            var items = connection.Query<CareerItem, Course, CareerItem>(sql, (careerItem, course) =>
+            {
+                careerItem.Course = course;
+                return careerItem;
+            }, splitOn: "Id");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Title} - Curso: {item.Course?.Title}");
             }
         }
     }
