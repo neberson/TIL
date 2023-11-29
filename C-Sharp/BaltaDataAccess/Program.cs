658cbae28cd359e6f93d8733bb68ab91a2441427
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-28 23:06:16 -0300

    Realizado a implementação de transaction

--- C-Sharp/BaltaDataAccess/Program.cs
+++ C-Sharp/BaltaDataAccess/Program.cs
@@ -29,7 +29,8 @@ namespace BaltaDataAccess
                 //OneToMany(connection);
                 //QueryMultiple(connection);
                 //SelectIn(connection);
-                Like(connection, "api");
+                //Like(connection, "api");
+                Transaction(connection);
             }
         }
 
@@ -334,6 +335,48 @@ namespace BaltaDataAccess
             {
                 Console.WriteLine(item.Title);
             }
+        }
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+
+            category.Id = Guid.NewGuid();
+            category.Title = "Minha categoria que não";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                                    Category 
+                                VALUES(
+                                         @Id, 
+                                         @Title, 
+                                         @Url, 
+                                         @Summary, 
+                                         @Order, 
+                                         @Description, 
+                                         @Featured)";
+
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }, transaction);
+                transaction.Commit();
+                //transaction.Rollback();
+                Console.WriteLine($"{rows} linhas inseridas");
+            }
+
         }
     }
 }
