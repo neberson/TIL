e4e4ad001a91cb5630be08a4b218a2817ff6c19f
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-28 22:41:15 -0300

    Realizado implementação do Select In

--- C-Sharp/BaltaDataAccess/Program.cs
+++ C-Sharp/BaltaDataAccess/Program.cs
@@ -27,7 +27,8 @@ namespace BaltaDataAccess
                 //ReadView(connection);
                 //OneToOne(connection);
                 //OneToMany(connection);
-                QueryMultiple(connection);
+                //QueryMultiple(connection);
+                SelectIn(connection);
             }
         }
 
@@ -302,6 +303,21 @@ namespace BaltaDataAccess
                 {
                     Console.WriteLine(item.Title);
                 }
+            }
+        }
+
+        static void SelectIn(SqlConnection connection)
+        {
+            var query = @"SELECT * FROM Career where Id IN @Id";
+
+            var items = connection.Query<Career>(query, new
+            {
+                Id = new[] { "01ae8a85-b4e8-4194-a0f1-1c6190af54cb", "e6730d1c-6870-4df3-ae68-438624e04c72" }
+            });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Title);
             }
         }
     }
