7fb10d44e381f8ce114e67f14612c668be728fe0
Neberson Andrade <neberson_mtv@hotmail.com>
2023-11-29 23:38:15 -0300

    Criação do projeto Blog e adicionado os pacotes SqlClient e Dapper

---
+++ C-Sharp/BlogDataAccess/Program.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace BlogDataAccess
+{
+    public class Program
+    {
+        private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=SA;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
