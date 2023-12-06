941d18a885fb04c0624c41746e60942979382d4f
Neberson Andrade <neberson_mtv@hotmail.com>
2023-12-05 22:56:00 -0300

    Criação do CRUD de usuários

--- C-Sharp/BlogDataAccess/Program.cs
+++ C-Sharp/BlogDataAccess/Program.cs
@@ -10,7 +10,11 @@ namespace BlogDataAccess
         private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=SA;Password=1q2w3e4r@#$;TrustServerCertificate=True";
         static void Main(string[] args)
         {
-            ReadUsers();
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
         }
 
         public static void ReadUsers()
@@ -23,6 +27,65 @@ namespace BlogDataAccess
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Name = "Equipe balta.io",
+                Bio = "Equipe balta.io",
+                Email = "hello@balta.io",
+                Image = "https://.....",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 4,
+                Name = "Equipe | balta.io",
+                Bio = "Equipe de suporte balta.io",
+                Email = "hello@balta.io",
+                Image = "https://.....",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização realizada com sucesso");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(4);
+                connection.Delete<User>(user);
+                Console.WriteLine("Usuário deletado com sucesso");
             }
         }
     }
